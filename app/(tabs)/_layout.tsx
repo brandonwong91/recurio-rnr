8d7bdf563637779638d00b055f51bda81d1c3e20
Brandon Wong <brandonwongck@gmail.com>
2025-09-03 00:23:58 +0800

    feat: add NativeOnlyAnimatedView component for native animations
    - Introduced a new component `NativeOnlyAnimatedView` that conditionally renders an animated view based on the platform. It uses `react-native-reanimated` for native animations and falls back to rendering children directly on the web platform.
    
    chore: add @rn-primitives/dropdown-menu dependency
    
    - Updated package.json to include `@rn-primitives/dropdown-menu` version 1.2.0.
    
    chore: update pnpm-lock.yaml with new dropdown-menu dependency
    
    - Updated pnpm-lock.yaml to reflect the addition of `@rn-primitives/dropdown-menu` and its dependencies.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import { Tabs, useRouter } from "expo-router";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
-import { signOut } from "~/lib/supabase";
+import { signOut, supabase } from "~/lib/supabase";
 import {
   Settings,
   DollarSign,
@@ -9,23 +9,71 @@ import {
   Apple,
   CalendarDays,
 } from "lucide-react-native";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import { useColorScheme } from "~/lib/useColorScheme";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 
 export default function TabsLayout() {
   const router = useRouter();
+  const { toggleColorScheme } = useColorScheme();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    fetchUser();
+  }, []);
 
   async function handleSignOut() {
     await signOut();
     router.replace("/(auth)/login");
   }
 
+  const getInitials = (fullName: string | undefined) => {
+    if (!fullName) return "";
+    const names = fullName.split(" ");
+    const initials = names.map((name) => name.charAt(0)).join("");
+    return initials.toUpperCase();
+  };
+
   return (
     <Tabs
       screenOptions={{
         headerShown: true,
         headerRight: () => (
-          <Button onPress={handleSignOut} className="mr-2">
-            <Text>Sign Out</Text>
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="link" className="mr-2">
+                <Avatar alt={""}>
+                  <AvatarImage
+                    source={{ uri: user?.user_metadata.avatar_url }}
+                  />
+                  <AvatarFallback>
+                    {getInitials(user?.user_metadata.full_name)}
+                  </AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onPress={toggleColorScheme}>
+                <Text>Toggle Theme</Text>
+              </DropdownMenuItem>
+              <DropdownMenuItem onPress={handleSignOut}>
+                <Text>Sign Out</Text>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         ),
         headerTitle: "Recurio",
       }}
