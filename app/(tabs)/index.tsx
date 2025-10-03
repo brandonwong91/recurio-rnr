83cb6f4738781e6f3133ec7ed5eb3e4326756187
Brandon Wong <brandonwongck@gmail.com>
2025-10-04 02:16:01 +0800

    Try changing api route location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,7 +12,7 @@ export default function ChatScreen() {
   const { messages, sendMessage, status } = useChat({
     transport: new DefaultChatTransport({
       fetch: expoFetch as unknown as typeof globalThis.fetch,
-      api: generateAPIUrl("/api/chat"),
+      api: generateAPIUrl("/chat"),
     }),
   });
 
